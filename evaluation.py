4b568033ba4095264684f6eae21649b706948eca
Jayasurya17 <jayasurya1796@gmail.com>
2021-04-29 18:03:35 -0700

    Evaluation and SVD baseline performed

--- evaluation.py
+++ evaluation.py
@@ -1,20 +1,36 @@
 import pandas as pd
 
 # Returns list of unique user IDs in the test dataset
-def get_position(item_id_orig, items):
+def get_position(item_id_test, items):
 	for index, item_id in enumerate(items):
-		if item_id == item_id_orig:
+		if item_id in item_id_test:
 			return index + 1
-	return -1
+	return 0
 
-def evaluate_recommendation(recommendationMethod):
-	test_data = pd.read_csv("./test_stratify.csv")
-	rating_positions = []
-	for index, row in test_data.iterrows():
-		items = recommendationMethod(row)
-		items = list(items)
-		position = get_position(row['item_id'], items)
-		rating_positions.append(position)
-		print ("Test", index, ":", "Item ID", row['item_id'], "was recommended was your", position, "recommendation")
+def get_recommendations(userGroups, row, recommendationMethod, topValueCount):
+	if row.name > topValueCount - 1:
+		return 0
+	recommendations = recommendationMethod(row)
+	recommended_items = list(recommendations['item_id'])
 
-	return sum(rating_positions) / len(rating_positions)
+	user_data = userGroups.get_group(row['user_id'])
+	item_test = set(user_data['item_id'])
+	position = get_position(item_test, recommended_items)
+# 	print ("Test ID:", row.name + 1, "recommendation was found at position", position)
+	return position
+
+def evaluate_recommendation(recommendationMethod, topValueCount):
+	test_data = pd.read_csv("./test.csv")
+	if topValueCount == -1:
+		topValueCount = len(test_data)
+	user_groups = test_data.groupby(['user_id'])
+	test_data['evaluation'] = test_data.apply(lambda x: get_recommendations(user_groups, x, recommendationMethod, topValueCount), axis = 1)
+	test_data.to_csv('similarity.csv', index = False)
+	counts = test_data['evaluation'].value_counts()
+	number_of_predictions = len(test_data) - counts[0] 
+	return {
+        'average_of_recommendations': test_data['evaluation'].sum() / number_of_predictions, 
+        'number_of_recommendations': number_of_predictions,
+        'total_test_cases': topValueCount,
+        '% of recommendations':  number_of_predictions / topValueCount * 100
+    }
